61304f4774dde0b1e36436059909721f987761be
Илья Кинеев <i.kineev@argustelecom.ru>
2025-11-11 19:23:11 +0300

    add employeeCard

--- src/main/frontend/generated/vaadin.ts
+++ src/main/frontend/generated/vaadin.ts
@@ -3,8 +3,6 @@ import './vaadin-featureflags.js';
 import './index';
 
 import './vaadin-react.js';
-import 'Frontend/generated/jar-resources/vaadin-dev-tools/vaadin-dev-tools.js';
-
 import './theme-sandgame.global.generated.js';
 import { applyTheme } from './theme.js';
 applyTheme(document);
